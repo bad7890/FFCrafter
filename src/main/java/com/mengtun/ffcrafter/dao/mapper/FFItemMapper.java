1460ff9b3665b9a6e62063a756d4752253360085
pike <pike@tronsis.com>
2022-03-09 14:20:29 +0800

    add @mapper

--- src/main/java/com/mengtun/ffcrafter/dao/mapper/FFItemMapper.java
+++ src/main/java/com/mengtun/ffcrafter/dao/mapper/FFItemMapper.java
@@ -2,8 +2,10 @@ package com.mengtun.ffcrafter.dao.mapper;
 
 import com.mengtun.ffcrafter.entity.FFItem;
 import com.mengtun.ffcrafter.entity.example.FFItemExample;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+@Mapper
 public interface FFItemMapper extends BaseMapper<FFItem, FFItemExample, Integer>{
 
   String getNameById(@Param("itemId") Integer itemId);
--- src/main/java/com/mengtun/ffcrafter/dao/mapper/FFRecipeMapper.java
+++ src/main/java/com/mengtun/ffcrafter/dao/mapper/FFRecipeMapper.java
@@ -2,7 +2,9 @@ package com.mengtun.ffcrafter.dao.mapper;
 
 import com.mengtun.ffcrafter.entity.FFRecipe;
 import com.mengtun.ffcrafter.entity.example.FFRecipeExample;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface FFRecipeMapper extends BaseMapper<FFRecipe, FFRecipeExample, Integer>{
 
 }
