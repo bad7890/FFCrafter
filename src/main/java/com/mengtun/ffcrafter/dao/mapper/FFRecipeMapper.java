d323feb2fb18210137f5f923ea13fc4bc724c0a0
xdl1997 <254090565@qq.com>
2022-03-30 13:50:55 +0800

    新增配方表mapper

--- src/main/java/com/mengtun/ffcrafter/dao/mapper/FFRecipeMapper.java
+++ src/main/java/com/mengtun/ffcrafter/dao/mapper/FFRecipeMapper.java
@@ -1,10 +1,13 @@
 package com.mengtun.ffcrafter.dao.mapper;
 
+import cn.hutool.json.JSONObject;
 import com.mengtun.ffcrafter.entity.FFRecipe;
 import com.mengtun.ffcrafter.entity.example.FFRecipeExample;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface FFRecipeMapper extends BaseMapper<FFRecipe, FFRecipeExample, Integer>{
-
+    JSONObject getRecipeByItemId(@Param("itemId") Integer itemId);
+    JSONObject getRecipeByRecipeId(@Param("itemId") Integer itemId);
 }
